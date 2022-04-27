b65aad03acc042db0ab974f2ddbbb29e5a37e829
Alex Delgado <gadc1996@gmail.com>
2022-04-27 10:28:23 -0500

    laptop config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,15 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
-echo "Bars launched..."
+# Launch bar1 and bar2
+# echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+# polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+
